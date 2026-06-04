62a4a35b2a5ad6de6d808487a5c8d58cd0a06b22
LexManos <LexManos@gmail.com>
2026-06-04 11:20:10 -0700

    FG Legacy Bulk tests

--- _internal/fg7/minecraft/1.7.10/src/main/java/net/miencraftforge/test/TestMod.java
+++ _internal/fg7/minecraft/1.7.10/src/main/java/net/miencraftforge/test/TestMod.java
@@ -1,13 +1,13 @@
 package net.miencraftforge.test;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraftforge.common.ForgeModContainer;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Property;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,18 +16,19 @@ import java.nio.file.Files;
 
 @Mod(modid = "test")
 public class TestMod {
+    private static final Logger LOG = LogManager.getLogger();
     public TestMod() {
-        Property update_check = ForgeModContainer.getConfig().get(ForgeModContainer.VERSION_CHECK_CAT, "Global", true);
-        update_check.set(false);
-        // Only register out handler on the server so i can test loading into the full world
+        ForgeModContainer.disableVersionCheck = true;
+        LOG.info("Loaded: " + FMLCommonHandler.instance().getSide());
+        // Only register out handler on the server so I can test loading into the full world
         if (FMLCommonHandler.instance().getSide() == Side.SERVER)
-            MinecraftForge.EVENT_BUS.register(this);
+            FMLCommonHandler.instance().bus().register(this);
     }
 
     @SubscribeEvent
     public void onServerTick(TickEvent.ServerTickEvent event) {
         if (event.phase != TickEvent.Phase.END) return;
-        System.out.println("SERVER_TICK_SUCCESS");
+        LOG.info("SERVER_TICK_SUCCESS " + new File("test_marker.txt").getAbsolutePath());
         try {
             Files.write(new File("test_marker.txt").toPath(), "SERVER_TICK_SUCCESS".getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
--- _internal/fg7/minecraft/1.8.9/src/main/java/net/miencraftforge/test/TestMod.java
+++ _internal/fg7/minecraft/1.8.9/src/main/java/net/miencraftforge/test/TestMod.java
@@ -8,6 +8,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,9 +18,11 @@ import java.nio.file.Files;
 
 @Mod(modid = "test")
 public class TestMod {
+    private static final Logger LOG = LogManager.getLogger();
     public TestMod() {
         Property update_check = ForgeModContainer.getConfig().get(ForgeModContainer.VERSION_CHECK_CAT, "Global", true);
         update_check.set(false);
+        LOG.info("Loaded: " + FMLCommonHandler.instance().getSide());
         // Only register out handler on the server so i can test loading into the full world
         if (FMLCommonHandler.instance().getSide() == Side.SERVER)
             MinecraftForge.EVENT_BUS.register(this);
@@ -27,7 +31,7 @@ public class TestMod {
     @SubscribeEvent
     public void onServerTick(TickEvent.ServerTickEvent event) {
         if (event.phase != TickEvent.Phase.END) return;
-        System.out.println("SERVER_TICK_SUCCESS");
+        LOG.info("SERVER_TICK_SUCCESS " + new File("test_marker.txt").getAbsolutePath());
         try {
             Files.write(new File("test_marker.txt").toPath(), "SERVER_TICK_SUCCESS".getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
